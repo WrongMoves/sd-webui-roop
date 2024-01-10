9550dbf9ee4e61f5c89619a392969b1f3c252445
WrongMoves <131088924+WrongMoves@users.noreply.github.com>
2024-01-11 10:33:31 +1300

    Update cimage.py

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -3,7 +3,8 @@ from ifnude import detect
 
 def convert_to_sd(img):
     shapes = []
-    chunks = detect(img)
+    min_prob = 2
+    chunks = detect(img, min_prob)
     for chunk in chunks:
         shapes.append(chunk["score"] > 2)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
